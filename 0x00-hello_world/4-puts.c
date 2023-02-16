9391be1ad4585aff83510cd2497b11ed08af75d6
Julian-Boateng <jayboateng44@gmail.com>
2023-02-16 12:17:15 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*Return: Always 0 (success)
-*/
+ * main - A program that prints a line with the puys function
+ * Return : 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+} 
+
