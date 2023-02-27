a02447150bc31769aab4c06f47b4a97bbb89e995
Julian-Boateng <jayboateng44@gmail.com>
2023-02-27 07:23:15 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	char *t = s;
+
+	{
+		while (*t)
+			t++;
+
+		while (s < --t)
+		{
+			*s ^= *t;
+			*t ^= *s;
+			*s ^= *t;
+			s++;
+		}
+	}
+}
+
