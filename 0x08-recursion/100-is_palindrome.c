d008a27582507ec16d83a15852e3da38516a3867
Julian-Boateng <jayboateng44@gmail.com>
2023-03-09 00:00:26 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,54 @@
 #include "main.h"
 
-int reverse(int num);
 /**
-* is_palindrome - checks if a string is a palindrome
-* @s: pointer
-* Return: 1 0r 0
-*/
-
+ * lenght - size of string
+ * @s: initial string
+ * Return: lenght
+ */
+int lenght(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (1 + lenght(s));
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * comp - compare two chars
+ * @s: string
+ * @size: lenght of s
+ * @inicio: variable to go through the string
+ * Return: 1 or 0 if it's palindrome
+ */
+int comp(char *s, int size, int inicio)
+{
+	if (s[inicio] == s[size - 1 - inicio])
+	{
+		if (inicio <= (size / 2))
+		{
+			return (comp(s, lenght(s), inicio + 1));
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * is_palindrome - gives if it is prime number
+ * @s: number
+ * Return: 1 if is palindrome or 0 if not
+ */
 int is_palindrome(char *s)
 {
-	if (*s == reverse(*s))
-	{
-		return (1);
-	}
-	return (0);
+	return (comp(s, lenght(s), 0));
 }
 
-/**
-* reverse - print reverse function
-* @num: parameter passed
-* Return: int
-*/
-int reverse(int num)
-{
-	int rem;
-	int sum = 0;
-
-	if (num != 0)
-	{
-		rem = num % 10;
-		sum = sum + 10 + rem;
-
-		reverse(num / 10);
-	}
-
-	else
-	return (sum);
-
-	return (sum);
-}
