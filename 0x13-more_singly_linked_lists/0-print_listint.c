0ecc47877b85191ae7fad95c02d1e995b37bfea6
Julian-Boateng <jayboateng44@gmail.com>
2023-04-04 02:25:58 -0700

    TASK 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t i;
+	size_t i;
 
-for (i = 0; h != NULL; i++)
-{
-printf("%d\n", h->n);
-h = h->next;
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
-return (i);
-}
+
+
