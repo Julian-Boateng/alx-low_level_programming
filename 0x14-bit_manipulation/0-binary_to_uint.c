e60b9aec6f4ab8bdf55cb0ec23d080f3f9328477
Julian-Boateng <jayboateng44@gmail.com>
2023-04-08 09:39:01 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,22 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned integer.
- * @b: Pointer to a string containing a binary number.
+ * binary_to_uint - converts a binary number to an unsigned integer
+ * @b: a string containing a binary number
  *
- * Return: The unsigned integer representation of the binary number, or 0 if the
- *         string is NULL or contains non-binary characters.
+ * Return: the converted unsigned integer or 0 if an error occurs
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	unsigned int len = 0;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
 
-	/* Get the length of the string */
-	while (b[len] != '\0')
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+	for (; *b != '\0'; ++b) {
+		if (*b != '0' && *b != '1')
 			return (0);
-		}
-		len++;
-	}
-
-	/* Convert the binary string to an unsigned int */
-	for (unsigned int i = 0; i < len; i++)
-	{
-		result <<= 1;
-		if (b[i] == '1')
-		{
-			result += 1;
-		}
+		result = (result << 1) | (*b - '0');
 	}
 
 	return (result);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,31 +2,15 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-/* This function outputs a character to the standard output */
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
 
-/* This function converts a binary string to an unsigned integer */
-unsigned int binary_to_uint(const char *b);
-
-/* This function prints the binary representation of a number */
-void print_binary(unsigned long int n);
-
-/* This function retrieves the value of a bit at a specified index in a number */
-int get_bit(unsigned long int n, unsigned int index);
-
-/* This function sets the value of a bit at a specified index in a number to 1 */
-int set_bit(unsigned long int *n, unsigned int index);
-
-/* This function sets the value of a bit at a specified index in a number to 0 */
-int clear_bit(unsigned long int *n, unsigned int index);
-
-/* This function returns the number of bits that need to be flipped in n to get m */
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
-/* This function checks the endianness of the system */
-int get_endianness(void);
-
-#endif /* MAIN_H */
-
+#endif
